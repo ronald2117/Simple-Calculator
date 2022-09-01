a088d16a598c5475bff7fd574849b64a1fa59689
abel.ronald2117@gmail.com <abel.ronald2117@gmail.com>
2022-09-01 11:29:34 +0800

    rename number button id's, clear dummy numbers on outputs and put refference on buttons on JavaScript file.

---
+++ script.js
@@ -0,0 +1,17 @@
+const clearButton = document.querySelector('#clear-btn');
+const deleteButton = document.querySelector('#delete-btn');
+const numSevenButton = document.querySelector('#num-7-btn');
+const numEightButton = document.querySelector('#num-8-btn');
+const numNineButton = document.querySelector('#num-nine-btn');
+const divideButton = document.querySelector('#divide-btn');
+const numFourButton = document.querySelector('#num-4-btn');
+const numFiveButton = document.querySelector('#num-5-btn');
+const numSixButton = document.querySelector('#num-6-btn');
+const multiplyButton = document.querySelector('#multiply-btn');
+const subtractButton = document.querySelector('#subtract-btn');
+const pointButton = document.querySelector('#point-btn');
+const numZeroButton = document.querySelector('#num-0-btn');
+const equalButton = document.querySelector('#equal-btn');
+const addButton = document.querySelector('#add-btn');
+const allButtons = document.querySelectorAll('.buttons');
+console.log(allButtons)
