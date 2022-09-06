7dcd7a2e5edce4705736574db45f8080e78547a3
abel.ronald2117@gmail.com <abel.ronald2117@gmail.com>
2022-09-06 18:07:54 +0800

    initial design

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
 const clearButton = document.querySelector('#clear-btn');
 const deleteButton = document.querySelector('#delete-btn');
 const equalButton = document.querySelector('#equal-btn');
-const previousOperandTextElement = document.querySelector('#upper-output');
-const currentOperandTextElement = document.querySelector('#lower-output');
 const numberButton = document.querySelectorAll('.num-btn');
 const operatorButton = document.querySelectorAll('.operator-btn');
+const pointButton = document.querySelector('#point-btn');
+let previousOperandTextElement = document.querySelector('#upper-output');
+let currentOperandTextElement = document.querySelector('#lower-output');
 
-class Calculator {
-  constructor (currentOperandTextElement, previousOperandTextElement){
-    this.previousOperandTextElement = previousOperandTextElement;
-    this.currentOperandTextElement = currentOperandTextElement;
-    this.operation = '';
-  }
-  updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-  }
-  appendNumber(number){
-    this.currentOperand = this.currentOperand + number;
-  }
-  
-}
-
-const calculator = new Calculator(currentOperandTextElement, previousOperandTextElement);
+let previousOperand = '';
+let currentOperand = '';
+let currentOperator = undefined;
 
 numberButton.forEach((button) => {
   button.addEventListener('click', () => {
-    console.log(calculator.operand)
-    calculator.appendNumber(button.innerText);
-    calculator.updateDisplay();
+  if (currentOperand.length == 11) return
+  appendNumber(button.innerText)
+  updateDisplay();
   })
 })
+operatorButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    if(currentOperand == '' && button.innerText == "-" && !currentOperand.includes("-")) {
+      appendNumber("-");
+      updateDisplay();
+      return
+    }
+    if(currentOperand == '') return
+    if(currentOperand != "" && previousOperand != ""){
+      previousOperand = ` ${getProduct()}  ${button.innerText}`;
+      currentOperand = ""
+      updateDisplay();
+      currentOperator = button.innerText;
+      return
+    }
+    previousOperand = currentOperand + " " + button.innerText;
+    currentOperand = '';
+    updateDisplay();
+    currentOperator = button.innerText;
+  })
+})
+pointButton.addEventListener('click', () => {
+  if(currentOperand == "" || currentOperand.includes(".")) return
+  appendNumber(pointButton.innerText)
+  updateDisplay()
+})
+equalButton.addEventListener('click', () => {
+  if(currentOperand == ""  || previousOperand == "") return
+  previousOperandTextElement.innerText += ` ${currentOperand} =`
+  currentOperandTextElement.innerText = getProduct();
+  updateDisplay;
+})
+clearButton.addEventListener('click', clearDisplay)
+deleteButton.addEventListener('click', deleteNumber)
+
+function updateDisplay(){
+  currentOperandTextElement.innerText = currentOperand;
+  previousOperandTextElement.innerText = previousOperand;
+}
+function appendNumber(num){
+  currentOperand += num
+}
+function clearDisplay(){
+  currentOperand = ''
+  previousOperand = ''
+  updateDisplay();
+}
+function deleteNumber(){
+  currentOperand = currentOperand.slice(0, -1)
+  updateDisplay()
+}
+
+function getProduct(){
+  let convertedPreviousOperand = parseFloat(previousOperand);
+  let convertedCurrentOperand = parseFloat(currentOperand);
+  let product;
+  switch(currentOperator){
+    case "+":
+      product = convertedCurrentOperand +
+      convertedPreviousOperand;
+      break;
+    case "-":
+      product = convertedCurrentOperand -
+      convertedPreviousOperand;
+    break;
+    case "×":
+      product = convertedCurrentOperand *
+      convertedPreviousOperand;
+      break;
+    case "÷":
+      product = convertedPreviousOperand /
+      convertedCurrentOperand;
+    break;
+    default:
+      return
+  }
+  return product
+}
